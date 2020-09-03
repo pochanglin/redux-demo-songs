643ef47740c3db1c19f7425eaf2eb79c42e33112
PoChangLin <allen4353968@gmail.com>
2020-09-03 14:04:55 +0800

    adding provider

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songReducer = () => {
     return [
         { title: 'Crazy', duration: '1:28'},
@@ -12,3 +14,8 @@ const selectedSongReducer = (selectedSong=null, action) => {
     }
     return selectedSong;
 }
+
+export default combineReducers({
+    songs: songReducer,
+    selectedSong: selectedSongReducer
+});
